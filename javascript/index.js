27552cd05cc4443009b848c905fa709b060acb00
gabriel schiavo <gabriel@lewagon.org>
2019-10-03 14:15:58 -0300

    automatted commit

--- javascript/index.js
+++ javascript/index.js
@@ -2,12 +2,7 @@ function docReady(e) {
     // see if DOM is already available
     if (document.readyState === "complete" || document.readyState === "interactive") {
         // call on next available tick
-        console.log("I'm a true statement");
-        docReady(function() {
-  			  // DOM is loaded and ready for manipulation here
-    			console.log("DOM is ready!");
-    			setTimeout(e, 2);
-				});
+        // console.log("I'm a true statement");
         // setTimeout(e, 1);
     } else {
         document.addEventListener("DOMContentLoaded", e);
